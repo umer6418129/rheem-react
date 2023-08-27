2680e1c1326bb839ac63d715e469afd9633c8baf
umer <md.farooq.ansari8959149@gmail.com>
2023-08-27 18:40:27 +0500

    create slider static to dynamic

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ import About from './About'
 
 function Home(props) {
   return (
-    <div className='mt-3'>
-        <img src={require('./../assests/gifs/home.gif')} alt="" className='rounded mx-auto d-block img-fluid w-75 ' />
+    <div className=''>
+        {/* <img src={require('./../assests/gifs/home.gif')} alt="" className='rounded mx-auto d-block img-fluid w-75 ' /> */}
       <div>
         <Carousel></Carousel>
         <BLog></BLog>
--- src/components/micro-components/Carousel.js
+++ src/components/micro-components/Carousel.js
@@ -1,61 +1,74 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
 
 function Carousel(props) {
+    let url = "http://127.0.0.1:8000/api/";
+    const sliderObj = {
+        get: "get/slider",
+    }
+    const [slider, setSlider] = useState([]);
+    
+    const fetchSlide = async (url) => {
+        try {
+            const res = await fetch(url + sliderObj.get);
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await res.json();
+            console.log(data);
+            setSlider(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchSlide(url);
+    }, []);
+
     return (
         <div className='my-3'>
-            <div id="carouselExampleDark"
-                className=" carousel carousel-dark slide rounded mx-auto d-block align-items-center img-fluid mt-5"
-                data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active"
-                        aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3"
-                        aria-label="Slide 4"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4"
-                        aria-label="Slide 5"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5"
-                        aria-label="Slide 6"></button>
-                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="6"
-                        aria-label="Slide 7"></button>
+            <div
+                id="carouselExampleDark"
+                className="carousel carousel-light slide rounded mx-auto d-block align-items-center img-fluid mt-5"
+                data-bs-ride="carousel"
+            >
+                <div className="carousel-inner mx-auto rounded d-block">
+                    {slider.map((slide, index) => (
+                        <div
+                            className={`carousel-item ${index === 0 ? 'active' : ''}`}
+                            key={index}
+                            data-bs-interval="10000"
+                        >
+                            <img
+                                src={slide.img}
+                                className="d-block w-100 cs"
+                                alt="..."
+                                style={{ width: '100%', height: '300px', objectFit: 'cover' }}
+                            />
+                        </div>
+                    ))}
                 </div>
-                <div className="carousel-inner  mx-auto rounded d-block">
-                    <div className="carousel-item active" data-bs-interval="10000">
-                        <img src={require('../../assests/carousel-banners/main.banner (2).png')} className="d-block w-100 cs " alt="..."/>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src={require('../../assests/carousel-banners/central air.png')} className="d-block w-100 cs rounded mx-auto img-fluid" alt='carousel'/>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="3000">
-                        <img src={require('../../assests/carousel-banners/ductless banner.png')} className="d-block w-100 cs rounded mx-auto d-block img-fluid" alt='carousel'/>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="4000">
-                        <img src={require('../../assests/carousel-banners/hybrid.png')} className="d-block w-100 cs rounded mx-auto d-block img-fluid" alt='carousel'/>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="5000">
-                        <img src={require('../../assests/carousel-banners/floor mounted.png')} className="d-block w-100 cs rounded mx-auto d-block img-fluid" alt='carousel'/>
-
-                    </div>
-                    <div className="carousel-item" data-bs-interval="6000">
-                        <img src={require('../../assests/carousel-banners/casset ac.png')} className="d-block w-100 cs rounded mx-auto d-block img-fluid" alt='carousel'/>
-
-                    </div>
-                    <div className="carousel-item" data-bs-interval="7000">
-                        <img src={require('../../assests/carousel-banners/portable.png')} className="d-block w-100 cs rounded mx-auto d-block img-fluid" alt='carousel'/>
-                    </div>
-                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-Carousel.propTypes = {
-
-}
-
-export default Carousel
-
+export default Carousel;
